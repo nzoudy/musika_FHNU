9857a7773c5f3b6ba053923dbda63f72a5a6af83
Hermann Fomekong T <fomekherm@hotmail.com>
2015-03-18 22:43:44 +0100

    FrontEnd Library
    Added jquery and Bootstrap

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="no-js">
 <head>
     <meta charset="utf-8">
     <title><?php if(isset($title)){ echo $title.'-';} echo APPNAME; ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- JS -->
-    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
-    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+    <!-- IE9 -->
+    <script src="<?php echo URL; ?>js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
     <!-- CSS -->
+    <link href="<?php echo URL; ?>css/bootstrap.css" rel="stylesheet">
     <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+
+
 </head>
 <body>
     <!-- logo -->
