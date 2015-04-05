88b2bb9f2ed6845f8650823d6f07cc73ce16875f
Hermann Fomekong T <fomekherm@hotmail.com>
2015-04-05 18:42:07 +0200

    Table structure
    Create new table structure

--- db/tables.sql
+++ db/tables.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 22, 2015 at 11:23 PM
+-- Generation Time: Apr 05, 2015 at 06:40 PM
 -- Server version: 5.5.39
 -- PHP Version: 5.4.31
 
@@ -28,19 +28,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `song` (
 `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
   `artist` text NOT NULL,
   `track` text NOT NULL,
   `link` text
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `song`
 --
 
-INSERT INTO `song` (`id`, `artist`, `track`, `link`) VALUES
-(1, 'Dena dina', 'Cash, Diamond Ring, Swimming Pools', 'http://www.youtube.com/watch?v=r4CDc9yCAqE'),
-(2, 'Jessy Lanza', 'Kathy Lee', 'http://vimeo.com/73455369'),
-(3, 'Philip Miloko', 'Cameroon', 'https://www.youtube.com/watch?v=qZQScGy47_w');
+INSERT INTO `song` (`id`, `userid`, `artist`, `track`, `link`) VALUES
+(10, 20, 'Test artist manager', 'Track test', 'upload/liquido - liquido - narcotic.mp3');
 
 -- --------------------------------------------------------
 
@@ -57,15 +56,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `groupid` int(11) NOT NULL DEFAULT '6',
   `updated` int(11) DEFAULT NULL,
   `created` int(11) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `email`, `groupid`, `updated`, `created`) VALUES
-(20, 'test', 'bcc4a19de13afdf707a58e238b30658334ce824d', 'Test artist', 'testherm@gmail.com', 6, 1428163611, 1428163611),
+(20, 'test', 'bcc4a19de13afdf707a58e238b30658334ce824d', 'Test artist manager', 'testherm@gmail.com', 6, 1428182995, 1428163611),
 (21, 'adminmusika', 'aa035512fb483b907001a49b6d9147e3f70c8c55', 'Administrator', 'adminmusika@musika.com', 1, 1428163695, 1428163695);
+
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `song`
@@ -87,12 +90,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `song`
 --
 ALTER TABLE `song`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
